78d836faa147a15f7ca6b51fea7b9d8066ca5d16
mdtradeonline-commits <mdtrade.online@gmail.com>
2026-03-01 21:27:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 import telebot
 from telebot import types
 
-# ПРЯМАЯ ВСТАВКА БЕЗ ПЕРЕМЕННЫХ
-TOKEN = "8646275203:AAGh_s34uGHfrLu36x00YM-j91YIU63cnD4"
-
+TOKEN = "ТВОЙ_НОВЫЙ_ТОКЕН_ЗДЕСЬ" # Вставь сюда тот токен, который ты получил после /revoke
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -14,16 +12,28 @@ def start(message):
     btn_en = types.InlineKeyboardButton("🇬🇧 English", callback_data='en')
     btn_pl = types.InlineKeyboardButton("🇵🇱 Polski", callback_data='pl')
     markup.add(btn_ru, btn_en, btn_pl)
-    bot.send_message(message.chat.id, "Choose language / Выберите язык / Wybierz język:", reply_markup=markup)
+    
+    bot.send_message(message.chat.id, "Select language / Выберите язык:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.data == "ru":
-        bot.send_message(call.message.chat.id, "🏠 Бот запущен на русском!")
-    elif call.data == "en":
-        bot.send_message(call.message.chat.id, "🏠 Bot is running in English!")
-    elif call.data == "pl":
-        bot.send_message(call.message.chat.id, "🏠 Bot działa po polsku!")
+    # Создаем кнопки со ссылками
+    links = types.InlineKeyboardMarkup(row_width=2)
+    funda = types.InlineKeyboardButton("🏠 Funda", url="https://www.funda.nl/")
+    pararius = types.InlineKeyboardButton("🔑 Pararius", url="https://www.pararius.com/")
+    kamernet = types.InlineKeyboardButton("🛌 Kamernet", url="https://kamernet.nl/")
+    links.add(funda, pararius, kamernet)
 
-print("--- СИСТЕМА ЗАПУЩЕНА ---")
+    if call.data == "ru":
+        text = "🏠 **Привет, Охотник за жильем!**\n\nЯ помогу тебе найти квартиру в Нидерландах. Вот основные сайты для старта:"
+    elif call.data == "en":
+        text = "🏠 **Hello, Housing Hunter!**\n\nI'll help you find a place in NL. Here are the main websites to start:"
+    else:
+        text = "🏠 **Cześć, Łowco Mieszkań!**\n\nPomogę Ci znaleźć mieszkanie w Holandii. Oto główne strony na start:"
+
+    # Удаляем кнопки выбора языка и шлем новый текст с полезными ссылками
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, 
+                         text=text, reply_markup=links, parse_mode='Markdown')
+
+print("--- БОТ-ОХОТНИК ЗАПУЩЕН ---")
 bot.infinity_polling()
