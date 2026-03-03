c1262872a60d53d8275678f3f76b4ea3bace144f
mdtradeonline-commits <mdtrade.online@gmail.com>
2026-03-03 22:14:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-# Берем токен из переменных окружения
+# ВСТАВЬ СВОЙ ТОКЕН НИЖЕ В КАВЫЧКИ (БЕЗ ПРОБЕЛОВ)
 API_TOKEN = '8646275203:AAFenGqJIBpvk1DXrbBqDIOPiOILz3Zyllg'
 
-# Проверка: если токен не нашелся, бот напишет об этом в логи
-if not API_TOKEN:
-    print("ОШИБКА: Переменная BOT_TOKEN не найдена в системе!")
-else:
-    print(f"Токен найден! Начинается на: {API_TOKEN[:5]}...")
-
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Я ЖИВОЙ! На связи!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
